4ca088e34743b9149de9364175703f7ca341a633
Stachu82 <stachelek.l@gmail.com>
2020-05-02 22:39:19 +0200

    Add GUI

--- app/src/main/java/eu/learn/firstcalculator/MainActivity.kt
+++ app/src/main/java/eu/learn/firstcalculator/MainActivity.kt
@@ -2,11 +2,20 @@ package eu.learn.firstcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun onDigit(view : View){
+
+        Toast.makeText(this, "Wciśnięto przycisk ", Toast.LENGTH_SHORT).show()
+
     }
 }
